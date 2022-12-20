03129056ff25efaa54bc1d98db8c61a40adbeae4
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2022-12-20 23:40:19 +0100

    e

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <script setup>
+import { reactive, ref } from 'vue';
+
 import Patches from "./components/Patches.vue"
 const a = "b"
+
+function parsePatch(name, patch){
+
+}
+
+function parsePatchValue(name, patchValue){
+
+}
+
+function parsePatchComponent(component, patchValue){
+
+}
+
+function parseInject(filename, tabs){
+
+}
+
 </script>
 
 <template>
