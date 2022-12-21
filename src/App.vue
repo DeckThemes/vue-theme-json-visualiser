ceef258760fbfb03a6f4ea2557768677241a1269
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2022-12-21 22:29:51 +0100

    i don't know anymore at this point

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,113 @@
 <script setup>
 import { reactive, ref } from 'vue';
 
-import Patches from "./components/Patches.vue"
-const a = "b"
-
-function parsePatch(name, patch){
-
+import Theme from "./components/Theme.vue"
+const data = {
+  manifest_version: 4,
+  injects: {
+    "shared.css": ["SP"]
+  }
 }
 
-function parsePatchValue(name, patchValue){
-
+function setProperty(from, to, key, example){
+    if (key in from)
+        to[key] = from[key]
+    else if (example)
+        to[key] = example
 }
 
-function parsePatchComponent(component, patchValue){
+function parseTheme(theme){
+    if (theme.manifest_version !== 4)
+        throw "Only manifest version 4 is supported"
 
+    let res = {
+        injects: [],
+        patches: []
+    }
+    
+    let common = {
+        "name": "My Epic Theme",
+        "author": "A Cool Person",
+        "target": "Other",
+        "version": "v1.2",
+        "description": ""
+    }
+
+    
+    for (const [key, value] of Object.entries(common))
+        setProperty(theme, res, key, value)
+
+    if ("injects" in theme)
+        res.injects = parseInjects(theme.injects)
+
+    if ("patches" in theme){
+        for (const [key, value] of Object.entries(theme.patches))
+            res.patches.push(parsePatch(key, value))
+    }
+
+    return res
 }
 
-function parseInject(filename, tabs){
+function parsePatch(key, value){
+    let components = ("components" in value) ? value.components : false
+    let defaultValue = ("default" in value) ? value.default : false
+    let values = []
+    for (const [k, v] of Object.entries(value.values)){
+        let c = []
 
+        if (components){
+            let i = components.find(x => x.on === k)
+            if (i)
+                c.push({
+                    componentName: i.name,
+                    type: i.type,
+                    default: i.default,
+                    css_variable: i.css_variable,
+                    tabs: i.tabs
+                })
+        }
+
+        values.push({
+            patchOptionName: k,
+            default: k === defaultValue,
+            injects: parseInjects(v),
+            components: c
+        })
+    }
+
+    return {
+        patchName: key,
+        type: ("type" in value) ? value.type : "dropdown",
+        values: values
+    }
 }
 
+function parseInjects(injects){
+    let items = []
+    for (const [key, value] of Object.entries(injects)) {
+        items.push({
+            filename: key,
+            tabs: value
+        })
+    }
+    return items
+}
+
+function newFile(e){
+  var file = e.target.files[0]; 
+  var reader = new FileReader();
+  reader.readAsText(file,'UTF-8');
+  reader.onload = readerEvent => {
+      var content = readerEvent.target.result; // this is the content!
+      console.log( content );
+      Object.assign(value, parseTheme(JSON.parse(content)))
+   }
+}
+
+let value = reactive(parseTheme(data))
 </script>
 
 <template>
-  <Patches />
+  <input type="file" @change="newFile($event)" />
+  <Theme :model-value="value" />
 </template>
--- src/components/Theme.vue
+++ src/components/Theme.vue
@@ -1,8 +1,59 @@
 <template>
-
+    <section class="theme">
+        <div>
+            <label :for="id+1">Theme Name:</label>
+            <input type="text" :id="id+1" v-model="modelValue.name" />
+        </div>
+        <div>
+            <label :for="id+2">Theme Author:</label>
+            <input type="text" :id="id+2" v-model="modelValue.author" />
+        </div>
+        <div>
+            <label :for="id+3">Theme Target:</label>
+            <select v-model="modelValue.target" :id="id+3">
+                <option v-for="x in targets" :value="x">{{ x }}</option>
+            </select>
+        </div>
+        <div>
+            <label :for="id+4">Theme Version:</label>
+            <input type="text" :id="id+4" v-model="modelValue.version" />
+        </div>
+        <div>
+            <label :for="id+5">Theme Description:</label>
+            <input type="text" :id="id+5" v-model="modelValue.description" />
+        </div>
+        <div>
+            <Injects v-model="modelValue.injects" />
+        </div>
+        <div>
+            <Patches v-model="modelValue.patches" />
+        </div>
+    </section>
 </template>
 
 <script setup>
+import { reactive, ref } from 'vue';
+import Injects from "./Injects.vue"
+import Patches from "./Patches.vue"
 
-
+const id = String(Math.random());
+const targets = [
+    "System-Wide",
+    "Background",
+    "Keyboard",
+    "Home",
+    "Background",
+    "Library",
+    "Store",
+    "Friends and Chat",
+    "Media",
+    "Downloads",
+    "Settings",
+    "Lock Screen",
+    "Tweak",
+    "Other",
+]
+const props = defineProps({
+    modelValue: Object
+})
 </script>
